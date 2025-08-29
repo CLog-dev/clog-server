918307a27a6c44206f1c0b536f53d851e5297373
hyomee2 <s5763305@naver.com>
2025-08-30 00:55:35 +0900

    initial commit

---
+++ src/main/java/com/hyomee/clog/ClogApplication.java
@@ -0,0 +1,13 @@
+package com.hyomee.clog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClogApplication.class, args);
+    }
+
+}
